30dd5639c4697dc965e5cf32bc2448c0ecac00bd
Karolina Tchiling <kdt@my.yorku.ca>
2025-01-18 11:31:56 -0500

    Establish custom colors

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        beige : '#FFFBF4',
+        green : '#CFE7C4',
+        blue : '#C4DDE7',
+        cream : '#E3C799',
+        shadow : 'rgba(227, 199, 153, 0.5)', 
+      },
+    },
+  },
+  plugins: [],
+}
+
