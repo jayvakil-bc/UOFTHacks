f7a75777329c44205fc7b6b4e3212e79943b698b
vickychenhubio <vicky.chen@hubio.com>
2025-01-18 01:30:42 -0500

    basic input

--- server.js
+++ server.js
@@ -11,6 +11,25 @@ app.get('/', (req, res) => {
   res.send('Hello, World!');
 });
 
+// POST Route - receiving form data
+app.post('/form-submit', (req, res) => {
+    const { restaurant_type, team_size, budget, price_point, location } = req.body;
+    if (!restaurant_type || !team_size || !budget || !price_point || !location) {
+      return res.status(400).send('Name and email are required.');
+    }
+    // Respond with the received data
+    res.status(200).json({
+        message: 'Received form data successfully!',
+        data: {
+            restaurant_type,
+            team_size,
+            budget,
+            price_point,
+            location
+        }
+    });
+  });
+  
 // Start the server
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
